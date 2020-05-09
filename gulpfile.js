5408dcf36d2180bb9f753fbce52cad2d52ecb111
ma2ken.gateway.ub20.04 <ma2ken.codingfan@gmail.com>
2020-05-09 12:02:32 +0900

    add direftory src dist

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+// ・src →参照元を指定するときに使う
+// ・dest →出力先を指定するときに使う
+// ・watch →ファイルを監視するときに使う
+// ・series（直列処理）とparallel（並列処理） →タスクを設定するときに使う
+const { src, dest, watch, series, parallel } = require( "gulp" );
+const sass = require( "gulp-sass" );
