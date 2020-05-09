fe85b838da661efdb12631776fe107c34146ad2c
ma2ken.gateway.ub20.04 <ma2ken.codingfan@gmail.com>
2020-05-09 12:05:06 +0900

    add sass　process

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,10 @@
 // ・series（直列処理）とparallel（並列処理） →タスクを設定するときに使う
 const { src, dest, watch, series, parallel } = require( "gulp" );
 const sass = require( "gulp-sass" );
+
+
+const cssSass = () => {
+    return src( 'src/css/**/*.scss' )
+        .pipe( sass() )
+        .pipe( dest( 'dist/css/' ) )
+ }
