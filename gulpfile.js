6faddc6a5514c504b0cda074585a6fad45c99da4
ma2ken.gateway.ub20.04 <ma2ken.codingfan@gmail.com>
2020-05-11 07:36:44 +0900

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,111 @@
-const { src, dest, watch, series, parallel } = require( "gulp" );
-const sass = require( "gulp-sass" );
-const plumber = require( "gulp-plumber" );
-const notify = require( "gulp-notify" );
-const postcss = require( "gulp-postcss" );
-const cssnext = require( "postcss-cssnext")
-const cleanCSS = require( "gulp-clean-css" );
-const rename = require( "gulp-rename" );
-const sourcemaps = require( "gulp-sourcemaps" );
+const { src, dest, watch, series, parallel } = require("gulp")
+const sass = require("gulp-sass")
+const plumber = require("gulp-plumber")
+const notify = require("gulp-notify")
+const postcss = require("gulp-postcss")
+const cssnext = require("postcss-cssnext")
+const autoprefixer = require("autoprefixer")
+const cleanCSS = require("gulp-clean-css")
+const rename = require("gulp-rename")
+const sourcemaps = require("gulp-sourcemaps")
 
-const babel = require( "gulp-babel" );
-const uglify = require( "gulp-uglify" );
+const babel = require("gulp-babel")
+const uglify = require("gulp-uglify")
 
-const imagemin = require( "gulp-imagemin" );
-const imageminMozjpeg = require( "imagemin-mozjpeg" );
-const imageminPngquant = require( "imagemin-pngquant" );
-const imageminSvgo = require( "imagemin-svgo" );
+const imagemin = require("gulp-imagemin")
+const imageminMozjpeg = require("imagemin-mozjpeg")
+const imageminPngquant = require("imagemin-pngquant")
+const imageminSvgo = require("imagemin-svgo")
 
-const browserSync = require( "browser-sync" );
+const browserSync = require("browser-sync")
 
 const browsers = [
-  'last 2 versions',
-  '> 5%',
-  'ie >= 11',
-  'not ie <= 10',
-  'ios >= 8',
-  'and_chr >= 5',
-  'Android >= 5',
+  "last 2 versions",
+  "> 5%",
+  "ie >= 11",
+  "not ie <= 10",
+  "ios >= 8",
+  "and_chr >= 5",
+  "Android >= 5",
 ]
 
 const srcPath = {
-  html: 'src/html/**/*',
-  css:  'src/css/**/*.scss',
-  js:   'src/js/**/*.js',
-  img:  'src/images/**/*',
+  html: "src/html/**/*",
+  css: "src/css/**/*.scss",
+  js: "src/js/**/*.js",
+  img: "src/images/**/*",
 }
 
 const destPath = {
-  html: 'dist/html/',
-  css:  'dist/css/',
-  js:   'dist/js/',
-  img:  'dist/images/',
+  html: "dist/html/",
+  css: "dist/css/",
+  js: "dist/js/",
+  img: "dist/images/",
 }
 
 const browserSyncOption = {
-  proxy: 'localhost',
+  proxy: "localhost",
   open: true,
   watchOptions: {
-    debounceDelay: 1000
+    debounceDelay: 1000,
   },
   reloadOnRestart: true,
 }
 
 const htmlCopy = () => {
-  return src(srcPath.html)
-    .pipe( destPath.html )
+  return src(srcPath.html).pipe(destPath.html)
 }
 
 const cssSass = () => {
-  return src( srcPath.css )
-    .pipe( sourcemaps.init() ) //gulp-sourcemapsを初期化
-    .pipe(
-      plumber( //エラーが発生しても処理は止めず
-        {
-            errorHandler: notify.onError( 'Error: <%= error.message %>' )
-            // エラー出力設定
-        }
-      )
-    )
-    .pipe( sass() )
-    .pipe( postcss( [cssnext(browsers)] ) ) //PostCSS
-    .pipe( dest( destPath.css ) ) //CSSを出力
-    .pipe( cleanCSS() ) //CSSを圧縮
-    .pipe(
-      rename(
-        { extname: '.min.css' } //ファイル名に min.css をつける
-      )
-    )
-    .pipe( sourcemaps.write( '/maps' ) ) //ソースマップを mapsディレクトリに出力
-  .pipe( dest( destPath.css ) ) //圧縮したCSSを出力
-  }
-
- const jsBabel = () => {
-  return src( srcPath.js )
+  return src(srcPath.css)
+    .pipe(sourcemaps.init()) //gulp-sourcemapsを初期化
     .pipe(
       plumber(
+        //エラーが発生しても処理は止めず
         {
-          errorHandler: notify.onError( 'Error: <%= error.message %>' )
+          errorHandler: notify.onError("Error: <%= error.message %>"),
+          // エラー出力設定
         }
       )
     )
-    .pipe( babel( {
-      presets: [ '@babel/preset-env' ]
-    } ) )
-    .pipe( dest( destPath.js ) )
-    .pipe( uglify() )
+    .pipe(sass())
+    .pipe(
+      postcss([
+        autoprefixer({
+          cascade: false,
+          grid: true,
+        }),
+      ])
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(destPath.css)) //CSSを出力
+    .pipe(cleanCSS()) //CSSを圧縮
     .pipe(
       rename(
-        { extname: '.min.js' }
+        { extname: ".min.css" } //ファイル名に min.css をつける
       )
     )
-    .pipe( dest( destPath.js ) )
-  }
+    .pipe(sourcemaps.write("/maps")) //ソースマップを mapsディレクトリに出力
+    .pipe(dest(destPath.css)) //圧縮したCSSを出力
+}
+
+const jsBabel = () => {
+  return src(srcPath.js)
+    .pipe(
+      plumber({
+        errorHandler: notify.onError("Error: <%= error.message %>"),
+      })
+    )
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(dest(destPath.js))
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(dest(destPath.js))
+}
 
 const imgImagemin = () => {
   return src(srcPath.img)
@@ -108,39 +113,42 @@ const imgImagemin = () => {
       imagemin(
         [
           imageminMozjpeg({
-            quality: 80
+            quality: 80,
           }),
           imageminPngquant(),
           imageminSvgo({
             plugins: [
               {
-                removeViewbox: false
-              }
-            ]
-          })
+                removeViewbox: false,
+              },
+            ],
+          }),
         ],
         {
-          verbose: true
+          verbose: true,
         }
       )
     )
-    .pipe( dest( destPath.img ) )
-  }
-
-const browserSyncFunc = () => {
-  browserSync.init( browserSyncOption );
+    .pipe(dest(destPath.img))
 }
 
-const browserSyncReload = ( done ) => {
-  browserSync.reload();
-  done();
+const browserSyncFunc = () => {
+  browserSync.init(browserSyncOption)
+}
+
+const browserSyncReload = (done) => {
+  browserSync.reload()
+  done()
 }
 
 const watchFiles = () => {
-  watch( srcPath.css, series( cssSass, browserSyncReload ) )
-  watch( srcPath.js, series( jsBabel, browserSyncReload ) )
-  watch( srcPath.img, series( imgImagemin, browserSyncReload ) )
-  watch( srcPath.html, series( htmlCopy, browserSyncReload ) )
+  watch(srcPath.css, series(cssSass, browserSyncReload))
+  watch(srcPath.js, series(jsBabel, browserSyncReload))
+  watch(srcPath.img, series(imgImagemin, browserSyncReload))
+  watch(srcPath.html, series(htmlCopy, browserSyncReload))
 }
 
-exports.default = series( series( cssSass, jsBabel, imgImagemin ), parallel( watchFiles, browserSyncFunc ) );
+exports.default = series(
+  series(cssSass, jsBabel, imgImagemin),
+  parallel(watchFiles, browserSyncFunc)
+)
